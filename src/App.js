186fb6f003b3c3bc86f40e718e18dd362f3b981a
Alina <alina220380@gmail.com>
2025-05-24 13:55:23 +0300

    update rooms

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./Main";
-import Registation from "./Registration";
+import Registration from "./Registration";
 import Login from "./Login";
 import Home from "./Home";
 import CreateRoom from "./CreateRoom";
 import Account from "./Account";
-
+import Rooms from "./Rooms"; 
 
 function App() {
   return (
-   <Home></Home>
-    <Account></Account>
-    <CreateRoom></CreateRoom>
     <Router>
       <Routes>
-        <Route path="/" element={<Main></Main>} />
-        <Route path="/registration" element={<Registation></Registation>} />
-        <Route path="/login" element={<Login></Login>} />
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Main />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/login" element={<Login />} /> */}
+        <Route path="/home" element={<Home />} /> {/* уникайте двох "/" */}
         <Route path="/rooms" element={<Rooms />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/create-room" element={<CreateRoom />} />
       </Routes>
     </Router>
-
   );
 }
 
 export default App;
-
